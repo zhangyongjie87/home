99ac74c9f55f108a008ea054a15a747d6869e0ce
zhangyongjie87 <869554122@qq.com>
2017-07-18 08:28:35 +0800

    first

---
+++ config/src/main/java/com/jmj/microservice/config/App.java
@@ -0,0 +1,13 @@
+package com.jmj.microservice.config;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
